4ff552e3e564cd6ac34dfe0630a82e83de577f4b
carlospanta1 <107951844+carlospanta1@users.noreply.github.com>
2022-12-28 08:11:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #se añade una lista de selección para poder elegir la fruta
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #se añadió una función para mostrar solo la fruta seleccionada en la lista
 fruits_to_show = my_fruit_list.loc[fruits_selected]
