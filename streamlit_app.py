91a6b8aa110554a7d48a868cbd71f7a977608f32
carlospanta1 <107951844+carlospanta1@users.noreply.github.com>
2023-01-01 23:28:06 -0500

    Update streamlit_app.py
    información de fruityvice como tabla

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #solo escribe la data en la pantalla
+
+#extrae la información de fruityvice
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+#muestra como tabla la información
+streamlit.dataframe(fruityvice_normalized)
 
 
 
