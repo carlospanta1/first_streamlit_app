e16545e57c08024bb8842c090239adc6608e52b1
carlospanta1 <107951844+carlospanta1@users.noreply.github.com>
2023-01-02 09:26:58 -0500

    Update streamlit_app.py
    codigo para agregar la fruta

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#codigo para agregar la fruta
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+
 
 
